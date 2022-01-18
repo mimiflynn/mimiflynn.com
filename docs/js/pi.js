6c202144c1df7bc8062bc3485380f440412db8ed
Mimi <mimiflynn@gmail.com>
2022-01-18 12:21:40 -0500

    shift control

--- docs/js/pi.js
+++ docs/js/pi.js
@@ -1,5 +1,12 @@
 (function () {
-    window.addEventListener('keydown', function (e) {
-        document.querySelector('p').innerHTML = `You pressed ${e.key}`;
-    }, false);
+    this.window.addEventListener('keydown', function (e) {
+        if (e.ctrlKey && e.shiftKey) {
+            console.log('ctrl + shift pressed');
+            this.document.querySelector('p').innerHTML = `You pressed magic combination`;
+            this.document.querySelector('.pi').addEventListener('click',function (e) {
+                console.log('pi was clicked with the magic combo');
+                this.document.querySelector('p').innerHTML = `You pressed magic combination AND CLICKED`;
+            }.bind(this), false);
+        }
+    }.bind(this), false);
 })();
